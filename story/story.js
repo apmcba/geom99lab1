8cd91f95d01b360752d95e29e601b5d3cbc3db96
apmcba <146374764+apmcba@users.noreply.github.com>
2024-01-22 14:59:05 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,37 +1,19 @@
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
-    center: { lat: 50.024, lng: -85.887 },
-  });
-  const citymap: Record<string, city> = {
-  bruce: {
-    center: { lat: 44.325278, lng: -81.599444 },
-    blast: 16,
-  },
-  darlington: {
-    center: { lat: 43.872778, lng: -78.719722 },
-    blast: 16,
-  },
-  pickering: {
-    center: { lat: 43.811667, lng: -79.065833 },
-    blast: 16,
-  },
-  point_lepreau: {
-    center: { lat: 45.068889, lng: -66.454722 },
-    blast: 16,
-  }
-    for (const city in citymap) {
-    const cityCircle = new google.maps.Circle({
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#FF0000",
-      fillOpacity: 0.35,
-      map,
-      center: citymap[city].center,
-      radius: Math.sqrt(citymap[city].population) * 100,
-    });
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-  });
-}
+// declare variables that will be used in this example
+    var myMap;                  
+    var centerpoint;            
+    var markers = [];           
+    var kmRadius = 16;          
+
+    /* Initialization function that sets up the map */
+    function initialize() {
+        // build the map's center poiint
+        centerpoint = new google.maps.LatLng(45.068889, -66.454722);
+        var mapOptions = {
+            zoom: 5, 
+            center: centerpoint
+        };
+
+        myMap = new google.maps.Map(
+            document.getElementById('map-canvas'), 
+            mapOptions
+        );
